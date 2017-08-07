56d4e905ed62e5166d4ec605ef59ae157e50af22
Joshua Thompson <ysovuka@gmail.com>
2017-08-07 17:16:16 -0500

    nUnit installation script.
    # Install a specific version of nUnit.
    powershell ./install-nunit.ps1 -version "3.7.1" -path "c:/nunit"

---
+++ install-nunit.ps1
@@ -0,0 +1,24 @@
+﻿param([String]$version="3.7.1",[String]$path="c:/nunit");
+
+$ProgressPreference = "SilentlyContinue";
+
+# nUnit version being bundled in this docker image.
+if ($version -eq "") {
+    $version = "3.7.1";
+}
+
+if ($path -eq "") {
+    $path = "c:/nunit";
+}
+
+New-Item -ItemType Directory -Force -Path "$path";
+
+Write-Host "";
+Write-Host "Downloading nUnit v$version to '$path'...";
+
+Invoke-WebRequest "https://github.com/nunit/nunit/archive/$version.zip" -OutFile "$env:TEMP/nunit.zip" -UseBasicParsing;
+Expand-Archive "$env:TEMP/nunit.zip" -DestinationPath "$path" -Force;
+dir "$env:TEMP" | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue;
+
+Write-Host "";
+Write-Host "Download complete.";
