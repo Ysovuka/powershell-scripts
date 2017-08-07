5d5ca873f5fdadeb307269bf7d9482f21e168518
Joshua Thompson <ysovuka@gmail.com>
2017-08-07 17:15:39 -0500

    NuGet installation script.
    # Install latest version of NuGet.
    powershell ./install-nuget.ps1 -path "c:/nuget"

---
+++ install-nuget.ps1
@@ -0,0 +1,22 @@
+﻿param([String]$path="c:/nuget");
+
+$ProgressPreference = "SilentlyContinue";
+
+if ($path -eq "") {
+    $path = "c:/nuget";
+}
+
+if (-not($env:NUGET_HOME)) {
+    $env:NUGET_HOME = $path;
+    [Environment]::SetEnvironmentVariable("NUGET_HOME", $env:NUGET_HOME, "Machine");
+}
+
+New-Item -ItemType Directory -Force -Path $path;
+
+Write-Host "";
+Write-Host "Downloading NuGet to '$path'...";
+
+Invoke-WebRequest "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" -OutFile "$path/nuget.exe" -UseBasicParsing;
+
+Write-Host "";
+Write-Host "Download complete.";
