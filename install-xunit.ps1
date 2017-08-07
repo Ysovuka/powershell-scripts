e3dd6add58ca717fbe4b1928cc9d9d34e389a56b
Joshua Thompson <ysovuka@gmail.com>
2017-08-07 17:16:46 -0500

    xUnit installation script.
    # Install a specific version of xUnit.
    powershell ./install-xunit.ps1 -version "2.2.0" -path "c:/xunit"

---
+++ install-xunit.ps1
@@ -0,0 +1,25 @@
+﻿param([String]$version = "2.2.0",[String]$path="c:/xunit");
+$ProgressPreference = "SilentlyContinue";
+
+# xUnit version being bundled in this docker image.
+if ($version -eq "") {
+    $version = "2.2.0";
+}
+
+if ($path -eq "") {
+    $path = "c:/xunit";
+}
+
+New-Item -ItemType Directory -Force -Path "$path";
+cd "$env:TEMP";
+
+Write-Host "";
+Write-Host "Installing xUnit v$version to '$path'...";
+
+Start-Process "$env:NUGET_HOME/nuget.exe" -ArgumentList "Install xunit.runner.console -Version $version" -PassThru | Wait-Process;
+mv "$env:TEMP/xunit.runner.console.$version/tools/*" "$path";
+dir "$env:TEMP" | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue;
+cd /;
+
+Write-Host "";
+Write-Host "Installation complete.";
